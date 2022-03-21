de5d01c53c21c1b0cae29772e96e2afaa6bd172e
Matthew-Jordon Whynter <mattwhynt@yahoo.com>
2022-03-20 19:09:46 -0500

    added max lengths to fields to avoid errors

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,10 @@ from flask_wtf.file import *
 
 class Create(FlaskForm):
 
-    title = StringField('Title', [InputRequired()])
+    title = StringField('Title', [InputRequired(), Length(max=80)])
     beds = IntegerField('Bedrooms', [InputRequired(), NumberRange(0)])
     baths = IntegerField('Bathrooms', [InputRequired(), NumberRange(0)])
-    location = StringField('Location', [InputRequired()])
+    location = StringField('Location', [InputRequired(), Length(max=255)])
     price = DecimalField('Price',[InputRequired()],places=2,rounding=2)
     type = SelectField('Type',[InputRequired()],choices=[('House','House'), ('Apartment','Apartment')])
     description = TextAreaField('Description',[DataRequired(), Length(max=255)])
